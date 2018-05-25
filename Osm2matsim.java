aed531782a4e0ce058fb917c5b84f4820f711665
Gustavo Covas <gtcovas@gmail.com>
2018-05-25 11:34:25 -0300

    Update coordinate transformation (does it make geographical sense?)

--- Osm2matsim.java
+++ Osm2matsim.java
@@ -29,7 +29,7 @@ public class Osm2matsim {
          *
          */
         CoordinateTransformation ct =
-                TransformationFactory.getCoordinateTransformation(TransformationFactory.WGS84, TransformationFactory.WGS84_UTM33N);
+                TransformationFactory.getCoordinateTransformation(TransformationFactory.WGS84, TransformationFactory.WGS84_Albers);
 
         /*
          * First, create a new Config and a new Scenario. One always has to do this when working with the MATSim
